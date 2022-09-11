af2bd0622b9f7b892a5756fb94c184e6f60318a7
Vincent Almeda <vzire08@gmail.com>
2022-09-10 20:05:27 -0400

    first commit

---
+++ Main/project1.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+#define TABLESIZE 21
+
+
+//hash function
+int hash(char *name, int key) {
+    int hashValue = 0;
+    hashValue = key % TABLESIZE;
+    return hashValue;
+} //end hash
+
+//struct for the user inputs
+typedef struct inputs {
+    char name;
+    int key
+
+} inputs; //end struct
+
+inputs *HashTable[TABLESIZE];
+
+//creates the hash table
+void initializeHashTable() {
+    int i;
+    for (i = 0; i < TABLESIZE; i++) {
+        HashTable[i] == NULL;
+    }
+
+    return;
+} //end initializeHashTable
+
+//prints the table
+void printTable() {
+    int i;
+    for (i = 0; i < TABLESIZE; i++) {
+        if (HashTable[i] == NULL) {
+            printf("%d\t___\n", i);
+        } else {
+            printf("%d\t%s\n", i, HashTable[i]->name);
+        }
+    }
+
+    return;
+}
+
+
+int main() {
+
+    initializeHashTable();
+    printTable();
+
+    return 0;
+}
