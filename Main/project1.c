0278a0571df4096fd719acbee1ff942ba5502268
Vincent Almeda <vzire08@gmail.com>
2022-09-14 09:27:30 -0400

    "Finished insert function"

--- Main/project1.c
+++ Main/project1.c
@@ -2,22 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 #define TABLESIZE 21
 
 //struct for the user inputs
 typedef struct inputs {
-    char name;
-    int key
+    char name[256];
+    int key;
 
 } inputs; //end struct
 
 inputs *HashTable[TABLESIZE];
 
 //hash function
-int hash(char *name, int key) {
+int hash(char *name) {
     int hashValue = 0;
-    hashValue = key % TABLESIZE;
+    int nameLength = strnlen(name, 256);
+    for (int i = 0; i < nameLength; i++) {
+        hashValue = (hashValue + name[i]) % TABLESIZE;
+    }
     return hashValue;
 } //end hash
 
@@ -30,6 +34,26 @@ void initializeHashTable() {
 
     return;
 } //end initializeHashTable
+
+bool hashInsert(inputs *person) {
+    if (person == NULL) {
+        printf("NULL POINTER\n");
+        return false;
+    }
+    int index = hash(person->name);
+
+
+    if (HashTable[index] != NULL) {
+        printf("Collision Occured\n");
+        return false;
+    }
+
+    HashTable[index] = person;
+    printf("Stored %s %d at location %d\n", person->name, person->key, index);
+    return true;
+
+    
+}
 
 //prints the table
 void printTable() {
@@ -48,8 +72,17 @@ void printTable() {
 
 int main() {
 
-    //initializes and prints the current table
+    //initializes the table
     initializeHashTable();
+
+    inputs jacob ={.name="Jacob", .key=25};
+    inputs jackie ={.name="Jackie", .key=45};
+    inputs chelsea ={.name="Chelsea", .key=30};
+
+    hashInsert(&jacob);
+    hashInsert(&jackie);
+    hashInsert(&chelsea);
+
     printTable();
 
     return 0;
