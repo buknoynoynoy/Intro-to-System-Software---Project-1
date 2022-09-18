1185d6a69cffbc4cb7fa4276ec58f7f3972d7ae3
Vincent Almeda <vzire08@gmail.com>
2022-09-17 20:35:11 -0400

    "added a check if someone already exists"

--- Main/project1.c
+++ Main/project1.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdint.h>
 #include <stdbool.h>
 
-#define TABLESIZE 21
+#define TABLESIZE 101
 
 //struct for the user inputs
 typedef struct inputs {
@@ -36,24 +35,28 @@ void initializeHashTable() {
 } //end initializeHashTable
 
 bool hashInsert(inputs *person) {
+    //checks if there is no input
     if (person == NULL) {
         printf("NULL POINTER\n");
         return false;
     }
     int index = hash(person->name);
-
-
-    if (HashTable[index] != NULL) {
-        printf("Collision Occured\n");
+    
+    //checks if the person already exists
+    if ((HashTable[index] != NULL)) {
+        if ((strncmp(HashTable[index]->name, person->name, 256) == 0)) {
+            printf("%s found at location %d with a value of %d\n", HashTable[index]->name, index, HashTable[index]->key);
+            return false;
+        } 
         return false;
     }
 
+    //if no one is there, it will insert the new person
     HashTable[index] = person;
     printf("Stored %s %d at location %d\n", person->name, person->key, index);
     return true;
 
-    
-}
+} //end hashInsert
 
 //prints the table
 void printTable() {
@@ -78,12 +81,14 @@ int main() {
     inputs jacob ={.name="Jacob", .key=25};
     inputs jackie ={.name="Jackie", .key=45};
     inputs chelsea ={.name="Chelsea", .key=30};
+    inputs jacob2 = {.name="Jacob"};
 
     hashInsert(&jacob);
     hashInsert(&jackie);
     hashInsert(&chelsea);
+    hashInsert(&jacob2);
 
-    printTable();
+    //printTable();
 
     return 0;
 }
